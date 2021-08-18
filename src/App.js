bb99c51393792bf51bf4a8a055a26e85623c1ea8
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-08-18 17:33:46 +0200

    elsy part 2 finish  7

--- src/App.js
+++ src/App.js
@@ -10,21 +10,36 @@ const stepsMin  = 0;
 const stepsMax  = 50000;
 
 export class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props) 
+    this.state = {
+       water: 0,
+       heart: 120,
+       temperature: -10,
+       steps : 3000      
+    }
+  }
+  onHeartChange = (e) => {
+    this.setState({heart: e.target.value})
+  }
+  onTempChange = (e) => {
+    this.setState({temperature: e.target.value})
+  }
+  onStepsChange = (e) => {
+    this.setState({steps: e.target.value})
+  }
+  render() {   
     return (
       <div className="container-fluid">
         <div className="row">
-        <p>Heart : {heartMax}</p>
-        <p>Temperature : {tempMin}</p>
-        <p>Steps : {stepsMin}</p>
           {/* Box Water */}
           <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L" />
           {/* Box Steps */}
-          <Box icon="directions_walk" color="black" value="3000" unit="steps"  />
+          <Box icon="directions_walk" color="black" value={this.state.steps} unit="steps" min={stepsMin} max={stepsMax} onChange={this.onStepsChange} />
           {/* Box Heart */}
-          <Box icon="favorite" color="red" value="120" unit="bpm" />
+          <Box icon="favorite" color="red" value={this.state.heart} unit="bpm" min={heartMin} max={heartMax} onChange={this.onHeartChange} />
           {/* Box Temperature */}
-          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C" />
+          <Box icon="wb_sunny" color="yellow" value={this.state.temperature} unit="°C" min={tempMin} max={tempMax} onChange={this.onTempChange} />
         </div>
       </div>
     );
--- src/components/Box.js
+++ src/components/Box.js
@@ -3,11 +3,18 @@ import React from "react";
 export class Box extends React.Component {
     render() {
         return(
+
         <div className="box col-sm-3 col-6">
-        <span className='material-icons' 
-        style={{color: this.props.color, fontSize: "100px"}}>
-        {this.props.icon}</span>
-        <p>{this.props.value}{this.props.unit}</p></div>
-        )
+            <span className='material-icons' 
+            style={{color: this.props.color, fontSize: "100px"}}>
+            {this.props.icon}</span>
+            <p>{this.props.value}{this.props.unit}</p>
+                {"local_drink"  !== this.props.icon &&
+                <input type="range" min={this.props.min} max={this.props.max} value={this.props.value} onChange= {(e)=> {
+                    this.props.onChange(e)
+                }}/>
+                }   
+        </div>
+       )
         }}
 export default Box;
